b6de9b632df036b4a2000e38ac2c334a78e77d85
Kenny Yang <kenny.j.yang@gmail.com>
2020-05-26 14:13:27 -0500

    update properties loading

--- src/main/java/sdg/demo/java/MysqlCon.java
+++ src/main/java/sdg/demo/java/MysqlCon.java
@@ -1,17 +1,37 @@
 package sdg.demo.java;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.sql.*;
+import java.util.Properties;
 
 class MysqlCon {
 	Connection con;
 	private PreparedStatement preparedStatement = null;
+	
 	public MysqlCon() {
 		try {
+			Properties prop = new Properties();
+			String propFileName = "config.properties";
+ 
+			InputStream inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
+ 
+			if (inputStream != null) {
+				prop.load(inputStream);
+			} else {
+				throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
+			}
+			String user = prop.getProperty("dbusr");
+			String password = prop.getProperty("dbpassword");
+			String dburl = prop.getProperty("dburl");
+			
+			
 			Class.forName("com.mysql.jdbc.Driver");
-			con = DriverManager.getConnection("jdbc:mysql://0.0.0.0:3306/employee", "root", "root");
-		} catch (Exception e) {
-			System.out.println(e);
+			con = DriverManager.getConnection(dburl, user, password);
+		} catch(Exception e) {
+			e.printStackTrace();
 		}
+			
 	}
 	
 	public String searchEmpTitle(String name) {
